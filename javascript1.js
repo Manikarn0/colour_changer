2eb26beb56c194f21da675d38f5def5f6fec96c8
Manikarn0 <150318472+Manikarn0@users.noreply.github.com>
2023-12-31 20:50:36 +0545

    Add files via upload

---
+++ javascript1.js
@@ -0,0 +1,10 @@
+function colourchanger(color) {
+    document.body.style.background= color;
+}
+function changeBodyBg(color){
+    document.body.style.background = color;
+}
+const heading = document.querySelector('heading');
+function changetitle(color){
+    document.getElementById("heading").innerHTML=` your colour is ${color}`;
+}
